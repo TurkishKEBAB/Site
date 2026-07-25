318c5b4bf5b8c508e961d9023ade25d5ad4fba61
Yigit Okur <yigitokur29@gmail.com>
2026-07-25 03:30:38 +0300

    chore(api): align generated frontend types

--- portfolio-project/frontend/src/services/apiTypes.generated.ts
+++ portfolio-project/frontend/src/services/apiTypes.generated.ts
@@ -1488,17 +1488,7 @@ export interface components {
             file: string;
         };
         /** C4DiagramData */
-        "C4DiagramData-Input": {
-            /**
-             * @description discriminator enum property added by openapi-typescript
-             * @enum {string}
-             */
-            kind: "c4";
-            /** Levels */
-            levels?: components["schemas"]["DossierC4LevelCreate"][];
-        };
-        /** C4DiagramData */
-        "C4DiagramData-Output": {
+        C4DiagramData: {
             /**
              * @description discriminator enum property added by openapi-typescript
              * @enum {string}
@@ -1743,7 +1733,7 @@ export interface components {
         /** DossierDiagramCreate */
         DossierDiagramCreate: {
             /** Data */
-            data: components["schemas"]["C4DiagramData-Input"] | components["schemas"]["SequenceDiagramData"] | components["schemas"]["SchemaDiagramData"] | components["schemas"]["TiersDiagramData"] | components["schemas"]["MatrixDiagramData"];
+            data: components["schemas"]["C4DiagramData"] | components["schemas"]["SequenceDiagramData"] | components["schemas"]["SchemaDiagramData"] | components["schemas"]["TiersDiagramData"] | components["schemas"]["MatrixDiagramData"];
             /**
              * Display Order
              * @default 0
@@ -1764,7 +1754,7 @@ export interface components {
         /** DossierDiagramResponse */
         DossierDiagramResponse: {
             /** Data */
-            data: components["schemas"]["C4DiagramData-Output"] | components["schemas"]["SequenceDiagramData"] | components["schemas"]["SchemaDiagramData"] | components["schemas"]["TiersDiagramData"] | components["schemas"]["MatrixDiagramData"];
+            data: components["schemas"]["C4DiagramData"] | components["schemas"]["SequenceDiagramData"] | components["schemas"]["SchemaDiagramData"] | components["schemas"]["TiersDiagramData"] | components["schemas"]["MatrixDiagramData"];
             /**
              * Display Order
              * @default 0
